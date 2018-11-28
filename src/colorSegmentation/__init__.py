31cbec6a6db4922d4a6ff38dc0a065923591963f
visittor <chattarin.rodpon@gmail.com>
2018-11-28 19:05:23 +0700

    [VISITTOR] Add function for colorConfigList to ColorDef object.

--- src/colorSegmentation/__init__.py
+++ src/colorSegmentation/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+from newbie_hanuman.msg import ColorDef
+
 def colorSegmentation( img, colorDef ):
 
 	marker = np.zeros( img.shape, dtype = np.int32 )
@@ -12,6 +14,25 @@ def colorSegmentation( img, colorDef ):
 		marker[ colorMask != 0 ] = color.ID 
 	
 	return marker
+
+def createColorDefFromDict( colorConfigList ):
+	colorDefList = []
+	for colorConfig in colorConfigList:
+		colorDef = ColorDef( 
+							ID = colorConfig["ID"],
+							Name = colorConfig["Name"],
+							RenderColor_RGB = colorConfig["RenderColor_RGB"],
+							# MinArea_pixels = colorConfig["MinArea_pixels"],
+							H_max = colorConfig["H_max"],
+							H_min = colorConfig["H_min"],
+							S_max = colorConfig["S_max"],
+							S_min = colorConfig["S_min"],
+							V_max = colorConfig["V_max"],
+							V_min = colorConfig["V_min"],
+		 					)
+		colorDefList.append( colorDef )
+	
+	return colorDefList
 
 def waterShed( img, marker ):
 	colorSeg = cv2.watershed( img, marker )
