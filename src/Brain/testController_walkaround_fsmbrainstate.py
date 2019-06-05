dea81def6e6b307dc87f72b38fcebd79740d9635
neverholiday <hy.nasrun@gmail.com>
2019-06-05 16:16:55 +0700

    Add test controller with walkaround

---
+++ src/Brain/testController_walkaround_fsmbrainstate.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2019  FIBO/KMUTT
+#			Written by EIEI
+#
+
+########################################################
+#
+#	STANDARD IMPORTS
+#
+
+import sys
+import os
+
+########################################################
+#
+#	LOCAL IMPORTS
+#
+
+from brain.brainState import FSMBrainState
+from brain.HanumanRosInterface import HanumanRosInterface
+
+from brain_script.gameControl_fsmbrainstate import Controller
+from brain_script.gameControl_brainstate import InitialState, ReadyState, SetState, PlayState, FinishState
+
+from brain_script.walk_around_fsmbrainstate import WalkAround
+
+from gotoandkick_fsmbrainstate import _IDLE, MainBrain
+
+########################################################
+#
+#	GLOBALS
+#
+
+########################################################
+#
+#	EXCEPTION DEFINITIONS
+#
+
+########################################################
+#
+#	HELPER FUNCTIONS
+#
+
+########################################################
+#
+#	CLASS DEFINITIONS
+#
+
+initialState = InitialState()
+readyState = ReadyState()
+setState = _IDLE()
+playState = WalkAround()
+finishState = _IDLE()
+penaltyState = _IDLE()
+
+main_brain = Controller(
+	initialState = initialState,
+	readyState = readyState,
+	setState = setState,
+	playState = playState,
+	finishState = finishState,
+	penaltyState = penaltyState
+)
