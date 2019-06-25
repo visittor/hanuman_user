3ecff4a33138edfca9e1bed9697ddfe81d151633
visittor <chattarin.rodpon@gmail.com>
2019-06-25 20:53:20 +0700

    Change standstill to (0,0,0).

--- src/Brain/gotoandkick_sub_brain/alignball_brainstate.py
+++ src/Brain/gotoandkick_sub_brain/alignball_brainstate.py
@@ -274,10 +274,10 @@ class RotateToTheBall2( FSMBrainState ):
 		
 	def stopRobotBehavior( self ):
 		#	stop
-		# self.rosInterface.LocoCommand( velX = 0.0,
-		# 			       			   velY = 0.0,
-		# 			       			   omgZ = 0.0,
-		# 			       			   commandType = 0,
-		# 			       			   ignorable = False )		
-		self.rosInterface.LocoCommand( command = "StandStill", commandType = 1, 
-									   ignorable =  False )
+		self.rosInterface.LocoCommand( velX = 0.0,
+					       			   velY = 0.0,
+					       			   omgZ = 0.0,
+					       			   commandType = 0,
+					       			   ignorable = False )		
+		# self.rosInterface.LocoCommand( command = "StandStill", commandType = 1, 
+		# 							   ignorable =  False )
