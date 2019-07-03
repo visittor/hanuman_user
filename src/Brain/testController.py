01c774363686650283025f6c48aa2fae8dd7aeee
neverholiday <hy.nasrun@gmail.com>
2019-07-04 00:09:44 +1000

    Update brain and change model

--- src/Brain/testController.py
+++ src/Brain/testController.py
@@ -23,6 +23,7 @@ from brain.HanumanRosInterface import HanumanRosInterface
 from brain_script.gameControl_fsmbrainstate import Controller
 from brain_script.gameControl_brainstate import InitialState, SetState, PlayState, FinishState, PernaltyState
 # from brain_script.gameControl_brainstate import ReadyState
+from brain_script.abnormal_brainstate import AbnormalState
 
 from enter_field.EnterField import ReadyState
 
@@ -56,11 +57,14 @@ playState = MainBrain()
 finishState = _IDLE()
 penaltyState = PernaltyState()
 
+abnormalState = AbnormalState()
+
 main_brain = Controller(
     initialState = initialState,
     readyState = readyState,
     setState = setState,
     playState = playState,
     finishState = finishState,
-    penaltyState = penaltyState
+    penaltyState = penaltyState,
+    abnormalState = abnormalState
 )
